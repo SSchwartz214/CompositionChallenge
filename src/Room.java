eeee8445bb49f95197b24ff44ef6072bb8edb073
Seth Schwartz <seth.schwartz@thoughtworks.com>
2019-10-28 14:08:20 -0500

    Adds Wall to Room

--- src/Room.java
+++ src/Room.java
@@ -2,10 +2,18 @@ public class Room {
 
     private Stove stove;
     private Table table;
+    private Wall wall1;
+    private Wall wall2;
+    private Wall wall3;
+    private Wall wall4;
 
-    public Room(Stove stove, Table table) {
+    public Room(Stove stove, Table table, Wall wall1, Wall wall2, Wall wall3, Wall wall4) {
         this.stove = stove;
         this.table = table;
+        this.wall1 = wall1;
+        this.wall2 = wall2;
+        this.wall3 = wall3;
+        this.wall4 = wall4;
     }
 
     public Stove getStove() {
